9040bef2c7195d49d41ad4130a468b83519009c0
Jason Hilder <jhilder95@gmail.com>
2025-04-15 10:55:23 +0200

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -90,6 +90,14 @@ vim.api.nvim_create_autocmd("TermOpen", {
 -- Easily hit escape in terminal mode.
 vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 
+-- Open a terminal at the bottom of the screen with a fixed height.
+vim.keymap.set("n", ",st", function()
+  vim.cmd.new()
+  vim.cmd.wincmd "J"
+  vim.api.nvim_win_set_height(0, 12)
+  vim.wo.winfixheight = true
+  vim.cmd.term()
+end)
 ---------------------------------------------------------------------------------
 -- [[ CONFIGS ]]
 ---------------------------------------------------------------------------------
